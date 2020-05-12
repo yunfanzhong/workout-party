667ce5152d34ac99cb212b93c34c21c244b0e119
franktzheng <ztcfrank123@gmail.com>
2020-05-12 12:10:24 -0700

    Set up MongoDB connection

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const { MONGO_URI, PORT } = process.env
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on('open', () => console.log(`MongoDB connection open! 🚀`))
+mongoose.connection.on('error', (err) =>
+  console.log(`Error connecting to MongoDB: ${err}`)
+)
+
+const app = express()
+
+app.get('/', (_req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
