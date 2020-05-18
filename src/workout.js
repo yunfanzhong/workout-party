d9e4502b5b569fe80e44859d6c610781fa076d7d
yunfanzhong <yunfanzhong.albany@gmail.com>
2020-05-17 23:11:05 -0700

    Added workout party, workout, exercise, and music schemas

---
+++ src/workout.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const workoutSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	startTime: Date,
+	music: mongoose.ObjectId,
+	exercises: [{
+		reps: Number,
+		exerciseID: mongoose.ObjectId
+	}]
+})
+
+module.exports = mongoose.model('Workout', workoutSchema)
---
+++ src/workoutParty.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const workoutPartySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	members: [mongoose.ObjectId],
+	workouts: [mongoose.ObjectId]
+})
+
+module.exports = mongoose.model('WorkoutParty', workoutPartySchema)
