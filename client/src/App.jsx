ff5c9f01f968764862082f2f1ea588e140357710
franktzheng <ztcfrank123@gmail.com>
2020-05-19 00:05:37 -0700

    Fixed App.js (because I messed it up earlier, whoops)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import { registerRootComponent } from 'expo'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import 'react-native-gesture-handler'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import AccountIcon from './assets/images/account_circle-24px.svg'
-import ChevronLeft from './assets/images/chevron_left-24px.svg'
-import NotificationIcon from './assets/images/notifications-24px.svg'
+
+import AccountIcon from '../assets/images/account_circle-24px.svg'
+import ChevronLeft from '../assets/images/chevron_left-24px.svg'
+import NotificationIcon from '../assets/images/notifications-24px.svg'
 import AccountScreen from './screens/AccountScreen.jsx'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LogInScreen from './screens/LogInScreen.jsx'
@@ -87,4 +89,4 @@ function App() {
   )
 }
 
-export default App
+export default registerRootComponent(App)
