45a2d506dae5d82e6f9a977f828752172f102249
eshah22 <eshahbazian22@gmail.com>
2020-05-20 17:12:40 -0700

    Friends List and Dynamic Account Information

--- client/src/context/UserProvider.jsx
+++ client/src/context/UserProvider.jsx
@@ -10,14 +10,61 @@ class UserProvider extends React.Component {
     user: null
   }
 
+  // handleLogin = async () => {
+  //   const { id: facebookID } = await logInWithFacebook()
+  //   try {
+  //     const user = await API.getUser(facebookID)
+  //     this.setState({ user })
+  //   } catch (err) {
+  //     Alert.alert('uwu', 'oopsies! we had a pwobwem wogging you in. 😔')
+  //   }
+  // }
+
   handleLogin = async () => {
-    const { id: facebookID } = await logInWithFacebook()
-    try {
-      const user = await API.getUser(facebookID)
-      this.setState({ user })
-    } catch (err) {
-      Alert.alert('uwu', 'oopsies! we had a pwobwem wogging you in. 😔')
-    }
+    this.setState({
+      user: {
+        facebookID: '10923840981234',
+        id: '4',
+        username: 'jk.jewik',
+        displayName: 'JSON Jewik',
+        lastLoggedIn: new Date(),
+        workoutHistory: [
+          { name: 'Legs', id: '10934802384' },
+          { name: 'Arms', id: '05829340239' },
+          { name: 'Chest', id: '10934802384' },
+          { name: 'Legs', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Cardio', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Back', id: '10934802384' },
+          { name: 'Legs', id: '10934802384' },
+          { name: 'Arms', id: '05829340239' },
+          { name: 'Chest', id: '10934802384' },
+          { name: 'Legs', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Cardio', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Back', id: '10934802384' },
+          { name: 'Legs', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Cardio', id: '10934802384' },
+          { name: 'Legs', id: '05829340239' },
+          { name: 'Back', id: '10934802384' },
+          { name: 'Core', id: '05829340239' }
+        ],
+        friendsList: [
+          { name: 'Ethan Shahbazian', id: '10934802384' },
+          { name: 'Yunfan Zhong', id: '05829340239' },
+          { name: 'Franklin Zheng', id: '10934802384' },
+          { name: 'David A Smallberg (DAS)', id: '10934802384' },
+          { name: 'Carey "The Goat" Nachenburg', id: '05829340239' },
+          { name: 'Daddy Block', id: '10934802384' },
+          { name: 'The Rock Johnson', id: '05829340239' },
+          { name: 'Paulus Tamagoman', id: '10934802384' },
+          { name: 'Emacs', id: '05829340239' }
+        ]
+      }
+    })
   }
 
   handleLogout = async () => {
