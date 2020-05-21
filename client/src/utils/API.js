7c41a6c11e52e4fdd20ec1a20fa349e937e7ce7b
franktzheng <ztcfrank123@gmail.com>
2020-05-21 16:19:25 -0700

    Updated API.js with routes, updated login process

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,14 +15,75 @@ function sleep(ms = 1000) {
   })
 }
 
+// Override Fetch API for debugging purposes
+async function fetch(...args) {
+  console.log('[DEBUG] Performing fetch with arguments:')
+  console.log(...args)
+  const res = await window.fetch(...args)
+  console.log(`[DEBUG] Performed fetch with response code ${res.status}`)
+  return res
+}
+
 const API = {
-  // Currently all test code
-  async getUser(facebookID) {
-    const res = await fetch(`${BASE_URL}/users/${facebookID}`, {
-      headers: this.headers
-    })
-    const user = await res.json()
+  // Untested - comment this out when you test it
+  async getUser(id) {
+    const res = await fetch(`${BASE_URL}/users/${id}`)
+    const user = res.json()
     return user
+  },
+
+  // Returns an object with the fields
+  // - user: a user object if the request was successful
+  // - doesNotExist: bool object specifying if a 404 occurred
+  // doesNotExist field lets the login process know that we need to create a
+  // new user. (sorry for the bad design)
+  async getUserByFacebookID(facebookID) {
+    const res = await fetch(`${BASE_URL}/facebook/${facebookID}`)
+    if (res.status === 404) {
+      return { doesNotExist: true, user: null }
+    } else if (!res.ok) {
+      throw new Error()
+    }
+    const user = await res.json()
+    return { user, doesNotExist: false }
+  },
+
+  // Untested - comment this out when you test it
+  // userInfo: { username, displayName, facebookID }
+  async createUser(userInfo) {
+    const res = await fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify(userInfo)
+    })
+    const createdUser = res.json()
+    return createdUser
+  },
+
+  // Untested - comment this out when you test it
+  async logWorkoutToUser(userID, workoutID) {
+    await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify({ workoutID })
+    })
+  },
+
+  // Untested - comment this out when you test it
+  async addFriendToUser(userID, friendUsername) {
+    await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify({ username: friendUsername })
+    })
+  },
+
+  // Untested - comment this out when you test it
+  // updates: { username: optional, displayName: optional }
+  async updateUser(userID, updates) {
+    const res = await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify(userInfo)
+    })
+    const createdUser = res.json()
+    return createdUser
   }
 }
 
