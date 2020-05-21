82b9001742b9160e39f342e901417b11f140fdbd
Jason Jewik <jj11d7t@gmail.com>
2020-05-21 11:19:33 -0700

    Upcoming Events List

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -4,10 +4,11 @@ import {
   Directions,
   State
 } from 'react-native-gesture-handler'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native'
 import UserContext from '../context/UserContext'
 import { useNavigation, NavigationContainer } from '@react-navigation/native'
 import ListItem from '../components/ListItem.jsx'
+import GroupIcon from '../../assets/images/group-24px.svg'
 
 function HeaderText(props) {
   return (
@@ -18,7 +19,8 @@ function HeaderText(props) {
         fontFamily: 'Roboto',
         fontSize: props.fontSize,
         fontWeight: 'bold',
-        paddingTop: props.paddingTop || 0
+        paddingTop: 15,
+        paddingBottom: 15
       }}
     >
       {props.text}
@@ -63,12 +65,45 @@ function UpcomingListItem(props) {
   return (
     <ListItem
       onPress={() => {
-        navigation.navigate('Event', { partyName: props.text })
+        navigation.navigate('Event', { partyName: props.partyName })
       }}
     >
-      <Text>{props.text}</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginLeft: '10%',
+          alignItems: 'center'
+        }}
+      >
+        <GroupIcon width={40} height={40} marginRight={10} fill="black" />
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <Text
+            style={{
+              fontFamily: 'Roboto',
+              fontSize: 18,
+              textAlign: 'left'
+            }}
+          >
+            {props.partyName}
+          </Text>
+          <Text>{props.eventTime}</Text>
+        </View>
+      </View>
     </ListItem>
   )
+}
+
+function UpcomingList(props) {
+  const upcomingList = props.upcomingList
+  const list = upcomingList.map((event) => (
+    <UpcomingListItem
+      key={event.id}
+      partyName={event.partyName}
+      eventTime={event.time}
+    />
+  ))
+  return <ScrollView>{list}</ScrollView>
 }
 
 function HomeScreen({ navigation }) {
@@ -88,14 +123,52 @@ function HomeScreen({ navigation }) {
         }}
       >
         <View style={styles.container}>
-          <HeaderText
-            fontSize={32}
-            color="black"
-            paddingTop={15}
-            text="Upcoming"
+          <HeaderText fontSize={32} color="black" text="Upcoming" />
+          <UpcomingList
+            upcomingList={[
+              // MOCK DATA
+              {
+                id: 1,
+                partyName: '108 1/7 Revolution',
+                time: '9 AM, Tues - May 12, 2020'
+              },
+              {
+                id: 2,
+                partyName: 'IOB',
+                time: '7 PM, Tues - May 12, 2020'
+              },
+              {
+                id: 3,
+                partyName: '108 1/7 Revolution',
+                time: '9 AM, Wed - May 13, 2020'
+              },
+              {
+                id: 4,
+                partyName: 'IOB',
+                time: '7 PM, Wed - May 13, 2020'
+              },
+              {
+                id: 5,
+                partyName: '108 1/7 Revolution',
+                time: '9 AM, Thurs - May 14, 2020'
+              },
+              {
+                id: 6,
+                partyName: 'IOB',
+                time: '7 PM, Thurs - May 14, 2020'
+              },
+              {
+                id: 7,
+                partyName: '108 1/7 Revolution',
+                time: '9 AM, Fri - May 15, 2020'
+              },
+              {
+                id: 8,
+                partyName: 'IOB',
+                time: '7 PM, Fri - May 15, 2020'
+              }
+            ]}
           />
-          <UpcomingListItem text="108 1/7 Revolution" />
-          <UpcomingListItem text="IOB" />
         </View>
       </FlingGestureHandler>
     </View>
