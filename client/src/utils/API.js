d9c3288d30a53a0494f353492ce5d66acc656fa0
yunfanzhong <yunfanzhong.albany@gmail.com>
2020-05-26 22:40:40 -0700

    Added both workout party and workout routes to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,15 +15,144 @@ function sleep(ms = 1000) {
   })
 }
 
-const API = {
-  // Currently all test code
-  async getUser(facebookID) {
-    const res = await fetch(`${BASE_URL}/users/${facebookID}`, {
-      headers: this.headers
-    })
-    const user = await res.json()
-    return user
-  }
+// Override Fetch API for debugging purposes
+async function fetch(...args) {
+  console.log('[DEBUG] Performing fetch with arguments:')
+  console.log(...args)
+  const res = await window.fetch(...args)
+  console.log(`[DEBUG] Performed fetch with response code ${res.status}`)
+  return res
 }
+
+const API = {
+  // Untested - comment this out when you test it
+  async getUser(id) {
+    const res = await fetch(`${BASE_URL}/users/${id}`)
+    const user = res.json()
+    return user
+  },
+
+  // Returns an object with the fields
+  // - user: a user object if the request was successful
+  // - doesNotExist: bool object specifying if a 404 occurred
+  // doesNotExist field lets the login process know that we need to create a
+  // new user. (sorry for the bad design)
+  async getUserByFacebookID(facebookID) {
+    const res = await fetch(`${BASE_URL}/facebook/${facebookID}`)
+    if (res.status === 404) {
+      return { doesNotExist: true, user: null }
+    } else if (!res.ok) {
+      throw new Error()
+    }
+    const user = await res.json()
+    return { user, doesNotExist: false }
+  },
+
+  // Untested - comment this out when you test it
+  // userInfo: { username, displayName, facebookID }
+  async createUser(userInfo) {
+    const res = await fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify(userInfo)
+    })
+    const createdUser = res.json()
+    return createdUser
+  },
+
+  // Untested - comment this out when you test it
+  async logWorkoutToUser(userID, workoutID) {
+    await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify({ workoutID })
+    })
+  },
+
+  // Untested - comment this out when you test it
+  async addFriendToUser(userID, friendUsername) {
+    await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify({ username: friendUsername })
+    })
+  },
+
+  // Untested - comment this out when you test it
+  // updates: { username: optional, displayName: optional }
+  async updateUser(userID, updates) {
+    const res = await fetch(`${BASE_URL}/users/${userID}`, {
+      method: 'POST',
+      body: JSON.stringify(userInfo)
+    })
+    const createdUser = res.json()
+    return createdUser
+  }
+},
+
+
+
+// WORKOUT PARTIES
+async getWorkoutParty(id) {
+  const res = await fetch(`${BASE_URL}/workoutParty/${id}`)
+  const wp = res.json()
+  return wp
+},
+
+async createWorkoutParty(wpInfo) {
+  const res = await fetch(`${BASE_URL}/workoutParty`, {
+      method: 'POST',
+      body: JSON.stringify(wpInfo)
+    })
+  const wp = res.json()
+  return wp
+},
+
+async addWorkoutToParty(wpID, workoutID) {
+  await fetch(`${BASE_URL}/workoutParty/${wpID}`, {
+      method: 'POST',
+      body: JSON.stringify({ workoutID })
+    })
+},
+
+async addMemberToParty(wpID, memberID) {
+  await fetch(`${BASE_URL}/workoutParty/${memberID}`, {
+      method: 'POST',
+      body: JSON.stringify({ memberID }) // TEST THIS
+    })
+},
+
+async updateWorkoutParty(wpID, newInfo) {
+    const res = await fetch(`${BASE_URL}/users/${wpID}`, {
+      method: 'POST',
+      body: JSON.stringify(newInfo)
+    })
+    const newWP = res.json()
+    return newWP
+  },
+
+
+  // WORKOUTS
+async getWorkout(id) {
+  const res = await fetch(`${BASE_URL}/workouts/${id}`)
+  const workout = res.json()
+  return workout
+},
+
+async createWorkout(workoutInfo) {
+  const res = await fetch(`${BASE_URL}/workouts`, {
+      method: 'POST',
+      body: JSON.stringify(workoutInfo)
+    })
+  const workout = res.json()
+  return workout
+},
+
+async updateWorkout(workoutID, newInfo) {
+    const res = await fetch(`${BASE_URL}/users/${workoutID}`, {
+      method: 'POST',
+      body: JSON.stringify(newInfo)
+    })
+    const newWorkout = res.json()
+    return newWorkout
+  },
+
 
 export default API
